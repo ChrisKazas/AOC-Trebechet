cdc561c259543b552070f2b25fc7c1492c71ac92
Chris Kazas <chriskazas.ck1@gmail.com>
2024-05-25 09:36:19 -0700

    Day 1
    --- Day 1: Trebuchet?! ---

---
+++ main.py
@@ -0,0 +1,32 @@
+
+
+
+def get_first(str):
+    for c in str:
+        if c.isnumeric():
+            return c
+        
+def get_last(str):
+    for c in reversed(str):
+        if c.isnumeric():
+            return c 
+arr = []
+
+with open("data.txt", 'r') as handle:
+    for row in handle:
+        word = row.rstrip()
+        
+        first = get_first(word)
+        last = get_last(word)
+        
+        num = int(first + last)
+        arr.append(num)
+
+
+result = sum(arr)
+print(result)
+        
+        
+    
+
+    
